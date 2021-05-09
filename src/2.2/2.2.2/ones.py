d72fe13158b4bf0aa6e198c901656c6a1fdcdd17
drs-t0m0 <drs-tomo@hotmail.co.jp>
2021-05-09 10:22:58 +0900

    commit 2.2.2

---
+++ src/2.2/2.2.2/ones.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print(np.ones(3))
+print("-" * 30)
+print(np.ones((2, 3)))
+print("#" * 30)
+
+print(np.ones(4, dtype="float32"))
+print("-" * 30)
+print(np.ones(4, dtype="int8"))
+print("-" * 30)
+print(np.ones((2, 3), dtype="complex"))
