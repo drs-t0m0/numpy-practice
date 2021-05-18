5d6d5c28e1519c50a6e5d91603c277f876b61dc3
drs-t0m0 <drs-tomo@hotmail.co.jp>
2021-05-18 09:00:02 +0900

    commit 3.2.4

---
+++ src/3.2/3.2.4/README.md
@@ -0,0 +1,2 @@
+# NumPyで階数（ランク）を求めるlinalg.matrix_rank関数の使い方
+
---
+++ src/3.2/3.2.4/matrix_rank.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+A = np.array([[1, 1, 4, 0, 1],
+              [0, 3, 1, 3, 2],
+              [1, 3, 0, 0, 1],
+              [2, 4, 3, 1, 1]])
+
+print(np.linalg.matrix_rank(A))
+print("-" * 30)
+
+B = np.array([[1, 2, 3, 0],
+              [2, 4, 6, 0],
+              [1, 0, 1, 2],
+              [1, 0, 0, 3]])
+
+print(np.linalg.matrix_rank(B))
